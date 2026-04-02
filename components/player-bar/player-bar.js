3105eb72aed699e4ed926e6bc1ff2ee5eb2fd8b6
Your Name <you@example.com>
2026-04-02 11:46:06 +0800

    feat: add player-bar component

---
+++ components/player-bar/player-bar.js
@@ -0,0 +1,50 @@
+Component({
+  properties: {
+    currentSound: {
+      type: Object,
+      value: null
+    },
+    isPlaying: {
+      type: Boolean,
+      value: false
+    },
+    remainingSeconds: {
+      type: Number,
+      value: 0
+    }
+  },
+
+  methods: {
+    onPlayTap() {
+      this.triggerEvent('playtoggle');
+    },
+
+    onTimerTap() {
+      this.triggerEvent('settimer');
+    },
+
+    onStopTap() {
+      this.triggerEvent('stop');
+    },
+
+    formatTime(seconds) {
+      const mins = Math.floor(seconds / 60);
+      const secs = seconds % 60;
+      return `${mins}:${secs < 10 ? '0' + secs : secs}`;
+    }
+  },
+
+  attached() {
+    this.setData({
+      hasTimer: this.data.remainingSeconds > 0
+    });
+  },
+
+  observers: {
+    remainingSeconds(val) {
+      this.setData({
+        hasTimer: val > 0
+      });
+    }
+  }
+});
